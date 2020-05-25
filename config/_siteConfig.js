10641c317c44f2463e0a95aa044eed50de93b617
jeffrey gaboni <mrgabonijeff@gmail.com>
2020-05-25 08:20:59 +0800

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -12,7 +12,7 @@ export default {
   siteName: 'Getbuya',
   tagline: 'All Things Digital',
   featureImage: '/uploads/home-hero.jpg',
-  logo: 'logo.png', // 'logo-component', // or '/logo.svg' for regular image
+  logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
     id: process.env.GOOGLE_ANALYTICS_ID
